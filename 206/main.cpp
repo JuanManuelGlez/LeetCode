ff1969274f9fd6bc2dafd8cd940bec4ef2476af9
JuanManuelGlez <a00572003@tec.mx>
2023-08-05 20:54:25 -0600

    reverse list in c++

---
+++ 206/main.cpp
@@ -0,0 +1,36 @@
+//Juan Manuel González Ascencio
+// comando para compilar: g++ main.cpp -o ./app
+// leetcode 206 from https://leetcode.com/problems/reverse-linked-list/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
+class Solution{
+    public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        ListNode* next = nullptr;
+        while (curr != nullptr){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+};
+
+int main (){
+
+}
